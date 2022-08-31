bc781a0eeddb15cc2ab162c6b2f12a1d001d7766
mistek131995 <mistek131995@gmail.com>
2022-08-31 06:29:18 +0300

    Добавьте файлы проекта.

---
+++ MCraftBlazor/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCraftBlazor.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Логин не может быть пустым.")]
+        [MinLength(5, ErrorMessage = "Минимальная длина логина 5 символов.")]
+        [MaxLength(12, ErrorMessage = "Максимальная длина логина 12 символов.")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Пароль не может быть пустым.")]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Адрес электронной почты не может быть пустым.")]
+        [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты.")]
+        public string Email { get; set; }
+    }
+}
